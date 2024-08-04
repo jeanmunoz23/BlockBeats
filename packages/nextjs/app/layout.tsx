a580c6eec54325fe74123386e39acf90f96c854c
gabimancini <gabimancinir@gmail.com>
2024-08-04 13:24:33 -0300

    Delete theme provider

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,6 +1,5 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
-import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
@@ -12,10 +11,8 @@ export const metadata = getMetadata({
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning>
-      <body>
-        <ThemeProvider enableSystem>
+      <body> 
           <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
-        </ThemeProvider>
       </body>
     </html>
   );

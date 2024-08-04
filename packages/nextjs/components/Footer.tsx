908f4fd3da92bd34b34bb914fffc4e58bd444225
gabimancini <gabimancinir@gmail.com>
2024-08-03 23:26:58 -0300

    Add smooth to back to the top event

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
   return (
     <footer className="min-h-0 py-10 px-10 mb-11 lg:mb-0">
       <h5>Shape the NFT Music</h5>
-      <div className="flex justify-between items-center w-full z-10  pointer-events-none">
+      <div className="flex justify-between items-center w-full z-10 ">
         <ul className="flex text-[32px]">
           <li className="pr-8 border-r border-white">Facebook</li>
           <li className="pr-8 pl-8 border-r border-white">Twitter</li>
